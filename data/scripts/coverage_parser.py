1fec16af6e3e12bbaf6f4f2000f06f496ba05dc4
mrowaha <ashfaqrowaha@gmail.com>
2025-04-12 18:43:16 +0300

    coverage_parser script

--- data/scripts/coverage_parser.py
+++ data/scripts/coverage_parser.py
@@ -1,3 +1,59 @@
 """
 includes functions to get the executed files and the executed lines
+must be executed from the data root folder
 """
+import argparse
+from pathlib import Path
+import json
+coverage_report_suffix = "coverage-report.json"
+executed_lines_suffix = "executed-lines.txt"
+executed_files_suffix = "executed-files.txt"
+
+
+def get_executed_lines(project_name: str, bug_id: int) -> None:
+    project_bug_dir = Path('.') / Path(project_name) / Path(str(bug_id))
+    report_filepath = project_bug_dir / Path(coverage_report_suffix)
+    result_filepath = project_bug_dir / Path(executed_lines_suffix)
+    with open(report_filepath) as f:
+        data = json.load(f)
+
+    with open(result_filepath, "w") as f:
+        for file, info in data.get("files", {}).items():
+            executed = set(
+                range(1, info["summary"]["num_statements"] + 1)) - set(info["missing_lines"])
+            f.write(f"{file}: Executed lines -> {sorted(executed)}\n")
+
+
+def get_executed_files(project_name: str, bug_id: int) -> None:
+    project_bug_dir = Path('.') / Path(project_name) / Path(str(bug_id))
+    report_filepath = project_bug_dir / Path(coverage_report_suffix)
+    result_filepath = project_bug_dir / Path(executed_files_suffix)
+    with open(report_filepath) as f:
+        data = json.load(f)
+
+    with open(result_filepath, "w") as f:
+        for file, info in data.get("files", {}).items():
+            f.write(f"{file}\n")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Generate executed lines and executed files for a given project name and its bug id")
+
+    parser.add_argument(
+        "--project",
+        type=str,
+        required=True,
+        help="project name"
+    )
+
+    parser.add_argument(
+        "--bug",
+        type=int,
+        required=True,
+        help="bug id"
+    )
+
+    args = parser.parse_args()
+    get_executed_lines(args.project, args.bug)
+    get_executed_files(args.project, args.bug)
